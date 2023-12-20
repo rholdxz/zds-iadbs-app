c1f269c9c9bbfe0e34f4c6774097ce325d38b34c
rholdxz <rholdxz@gmail.com>
2023-12-20 16:32:13 +0800

    Test then Create or Implement.

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculating Addition Test Operator
 def add(x, y):
     """Process the addition test operator."""
     return x + y
+
+
+def sub(x, y):
+    """Process the subtract test operator."""
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -12,4 +12,9 @@ class CalcTest(SimpleTestCase):
     def test_add_number(self):
         """Test function adding numbers."""
         res = calc.add(5, 5)
-        self.assertEqual(res, 10, msg="The sum is not correct")
+        self.assertEqual(res, 10, msg="The sum is not correct.")
+
+    def test_subtract_number(self):
+        """Test function subtract numbers."""
+        res = calc.sub(5, 5)
+        self.assertEqual(res, 0, msg="The subtract is not correct.")
