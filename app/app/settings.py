db43fc27d37f83e1a86130c872f5a67ffd31c1ba
rholdxz <rholdxz@gmail.com>
2023-12-23 16:08:30 +0800

    Admin list user and updating settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -207,8 +207,8 @@ JAZZMIN_SETTINGS = {
 
     # Additional links to include in the user menu on the top right ("app" url type is not allowed)
     "usermenu_links": [
-        {"name": "Developer", "url": "https://rholdxz.github.io", "new_window": True},
-        {"model": "auth.user"}
+        {"name": "Developer", "url": "https://rholdxz.github.io", "new_window": True, "icon": "fas fa-link"},
+        # {"model": "auth.user"}
     ],
 
     #############
@@ -231,25 +231,25 @@ JAZZMIN_SETTINGS = {
     # "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
 
     # Custom links to append to app groups, keyed on app name
-    "custom_links": {
-        "books": [{
-            "name": "Make Messages",
-            "url": "make_messages",
-            "icon": "fas fa-comments",
-            "permissions": ["books.view_book"]
-        }]
-    },
+    # "custom_links": {
+    #     "books": [{
+    #         "name": "Make Messages",
+    #         "url": "make_messages",
+    #         "icon": "fas fa-comments",
+    #         "permissions": ["books.view_book"]
+    #     }]
+    # },
 
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
     # for the full list of 5.13.0 free icon classes
     "icons": {
-        "auth": "fas fa-users-cog",
-        "auth.user": "fas fa-user",
+        "auth": "fas fa-user",
+        "core.User": "fas fa-user",
         "auth.Group": "fas fa-users",
     },
     # Icons that are used when one is not manually specified
-    "default_icon_parents": "fas fa-chevron-circle-right",
-    "default_icon_children": "fas fa-circle",
+    # "default_icon_parents": "fas fa-chevron-circle-right",
+    # "default_icon_children": "fas fa-circle",
 
     #################
     # Related Modal #
